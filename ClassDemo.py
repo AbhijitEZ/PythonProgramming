ee117e9578e4d7748d561cd7132b63514ef87a3b
Abhijit Ezhava <abhijit.nair659@gmail.com>
2019-05-23 19:01:40 +0530

    magic method

--- ClassDemo.py
+++ ClassDemo.py
@@ -4,6 +4,14 @@ class Car:
     def __init__(self, wheels_count=2):
         self.wheels_count = wheels_count  # property
 
+    def __str__(self):
+        # default function is when calling print
+        return(f"This is default print")
+
+    @classmethod
+    def zeroWheel(cls):  # class method, cls points to the class itself
+        return cls(0)
+
     def wheels(self, wheels_count):  # method
         self.wheels_count = wheels_count
         print(f"{wheels_count} number of wheels")
@@ -16,3 +24,6 @@ honda.default_value = "lamda"
 honda.wheels(15)
 another = Car(3)
 print(another.default_value)
+
+nullWheel = Car.zeroWheel()
+print(nullWheel)
