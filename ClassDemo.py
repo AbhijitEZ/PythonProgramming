6bb4606bbfe9418e3d9c87dcc8684adf33d29671
Abhijit Ezhava <abhijit.nair659@gmail.com>
2019-05-23 20:10:10 +0530

    Completed Object Oriented

--- ClassDemo.py
+++ ClassDemo.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 class Car:
     default_value = "All remain same"  # static
 
@@ -7,6 +10,10 @@ class Car:
     def __str__(self):
         # default function is when calling print
         return(f"This is default print")
+
+    def __eq__(self, other):
+        # equality check magic method for the object
+        return self.wheels_count == other.wheels_count
 
     @classmethod
     def zeroWheel(cls):  # class method, cls points to the class itself
@@ -19,6 +26,7 @@ class Car:
 
 Car.default_value = "changed"
 honda = Car(4)
+
 print(honda.default_value)
 honda.default_value = "lamda"
 honda.wheels(15)
@@ -27,3 +35,75 @@ print(another.default_value)
 
 nullWheel = Car.zeroWheel()
 print(nullWheel)
+
+# equality check magic method for the object
+jeep = Car(4)
+maruti = Car(4)
+print(jeep == maruti)
+
+
+# Container like representation in class
+class TagCloud:
+
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+
+cloud = TagCloud()
+cloud.add("Python")
+cloud.add("python")
+cloud.add("PythOn")
+print(cloud.tags)
+
+
+class Product:
+    def __init__(self, price):
+        self.__price = price
+
+    def getItem(self):
+        return self.__price   # private property
+
+    def setIntem(self, value):
+        self.__price = value
+
+
+dust = Product(10)
+print(dust.getItem())
+
+
+# inheritance
+class Animal:
+    def eat(self):
+        print("eat")
+
+
+class Human(Animal):
+    def walk(self):
+        print("walk")
+
+
+John = Human()
+John.walk()
+John.eat()
+
+
+# abstract
+class Mammal(ABC):
+    def eat(self):
+        print("FOOD")
+
+    @abstractmethod
+    def walk(self):
+        pass  # used when there is no implemetation
+
+
+class Bear(Mammal):
+    def walk(self):
+        print("With 4 legs")
+
+
+lupa = Bear()
+lupa.walk()
